0977d5fb0b5700478dead913c6fb0d581d1e3180
David Keegan <keegan3d@gmail.com>
2009-12-28 00:51:26 -0500

    This code was in InScopeLib before: http://bitbucket.org/keegan3d/inscopelib/changeset/a5573079bc47/

---
+++ PyGlang.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+'''
+Translate text with the 'Google AJAX Language API'
+http://code.google.com/apis/ajaxlanguage/documentation/
+'''
+
+import re, sys
+import urllib
+try:
+    #python2.6+
+    import json
+except ImportError:
+    #python2.5
+    import simplejson as json
+
+apiVersion  = '1.0'
+baseUrl     = 'http://ajax.googleapis.com/ajax/services/language/translate'
+
+class TranslationError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return str(self.value)
+
+#TODO: add language enum
+#TODO: add detect function
+
+def translate(text, fromLang=None, toLang='en'):
+    '''
+    Use the google language api to translate the given text from one language to another.
+    English is the default 'translate to' language, 
+    if no 'from language' is given google will guess what the language is.
+    
+    Example url:
+    http://ajax.googleapis.com/ajax/services/language/translate?langpair=en|ru&q=hello&v=1.0
+    '''
+    
+    #if there is nothing to translate or no language to tranlate to reutrn an empty unicode string
+    if len(text) == 0 or len(toLang) == 0:
+        return u''
+        
+    if fromLang == None:
+        fromLang = ''
+    
+    #setup the arguments for the url   
+    params = {}
+    params['langpair']  = '%s|%s' % (fromLang, toLang)
+    params['v']         = apiVersion
+    
+    #try to encode the text as utf-8
+    try:
+        params['q'] = text.encode('utf-8')
+    except UnicodeDecodeError:
+        params['q'] = text
+    
+    #get the tranlated text from google
+    jsonData = json.load(urllib.urlopen(baseUrl, data = urllib.urlencode(params)))
+    responseData = jsonData['responseData']
+    if responseData == None:
+        raise TranslationError(jsonData['responseDetails'])
+    else:
+        if responseData['translatedText'] != None:
+            return responseData['translatedText']
+            
+    return u''
